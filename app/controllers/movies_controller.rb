58594b9c0d20ad4c600ee0e18c8848190eddd744
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-29 15:36:31 -0400

    movies being saved, and displayed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,35 @@
 class MoviesController < ApplicationController
     def index
+        @movies = Movie.all 
     end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.create!(movie_params)
+
+        redirect_to "/"
+    end
+
+    def edit
+        @movie = Movie.find(params[:id])
+    end
+
+    def update
+        @movie = Movie.find(params[:id])
+        @movie.update(movie_params)
+
+        redirect_to 
+    end
+
+    def show
+        @movie = Movie.find(params[:id])
+    end
+
+    private
+    def movie_params
+        params.require(:movie).permit(:title, :genre, :year, :plot, :picture)
+end 
 end
